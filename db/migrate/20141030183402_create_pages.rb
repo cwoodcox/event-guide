f8bf22aa1fa9129379a5650553ac8bb44b8d3dd7
Corey Woodcox <corey.woodcox@gmail.com>
2014-10-30 14:35:15 -0400

    rails g model page
    title:string display_type:string display_order:integer content:text

---
+++ db/migrate/20141030183402_create_pages.rb
@@ -0,0 +1,12 @@
+class CreatePages < ActiveRecord::Migration
+  def change
+    create_table :pages do |t|
+      t.string :title
+      t.string :display_type
+      t.integer :display_order
+      t.text :content
+
+      t.timestamps
+    end
+  end
+end

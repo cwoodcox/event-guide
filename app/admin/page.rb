02d3f017dee993e7eba8d59f890c217b3cc8d6f6
Corey Woodcox <corey.woodcox@gmail.com>
2014-10-30 15:18:45 -0400

    register page as belonging to event

--- app/admin/page.rb
+++ app/admin/page.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register Page do
-
+  belongs_to :event
 
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
